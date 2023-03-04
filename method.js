8d186795de204b5b2394a1b45466d6c5f7b64ff5
lunarcwhite <rizkym058@gmail.com>
2023-03-04 10:14:31 +0700

    tugas

--- method.js
+++ method.js
@@ -1,19 +1,78 @@
-let nama = 10
+let nama, asal, universitas, fakultas, jurusan, semester, suku
+
+// 1
 function setNama(argNama) {
     nama = argNama
 }
+setNama('Muhamad Rizky Maulana')
 
-function getName(){
+function getNama(){
     return nama
 }
-console.log(getName())
+console.log(getNama())
 
-let asal = 'cianjur'
+// 2
 function setAsal(argAsal) {
     asal = argAsal
 }
+setAsal('Cianjur')
 
 function getAsal(){
     return asal
 }
 console.log(getAsal())
+
+// 3
+function setUniversitas(argUniversitas) {
+    universitas = argUniversitas
+}
+setUniversitas('Universitas Suryakancana')
+
+function getUniversitas(){
+    return universitas
+}
+console.log(getUniversitas())
+
+// 4
+function setFakultas(argFakultas) {
+    fakultas = argFakultas
+}
+setFakultas('Teknik')
+
+function getFakultas(){
+    return fakultas
+}
+console.log(getFakultas())
+
+// 5
+function setJurusan(argJurusan) {
+    jurusan = argJurusan
+}
+setJurusan('Teknik Informatika')
+
+function getJurusan(){
+    return jurusan
+}
+console.log(getJurusan())
+
+// 6
+function setSemester(argSemester) {
+    semester = argSemester
+}
+setSemester('6')
+
+function getSemester(){
+    return semester
+}
+console.log(getSemester())
+
+// 7
+function setSuku(argSuku) {
+    suku = argSuku
+}
+setSuku('Sunda')
+
+function getSuku(){
+    return suku
+}
+console.log(getSuku())
