505c5d3829a980b361aa1a5a60abb9bfdd57695f
lunarcwhite <rizkym058@gmail.com>
2023-03-04 09:59:27 +0700

    tugas method

---
+++ method.js
@@ -0,0 +1,19 @@
+let nama = 10
+function setNama(argNama) {
+    nama = argNama
+}
+
+function getName(){
+    return nama
+}
+console.log(getName())
+
+let asal = 'cianjur'
+function setAsal(argAsal) {
+    asal = argAsal
+}
+
+function getAsal(){
+    return asal
+}
+console.log(getAsal())
